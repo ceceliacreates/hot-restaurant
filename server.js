d461b44871dc7f03141a8c92303dd8bdf003bbc6
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-05-17 11:33:41 -0400

    updates make reservation form and fixes server.js to accept data

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 const reservations = [
     {
-        name: "bob",
+        fname: "bob",
+        lname: "smith",
         email: "bob@bob.com",
         phone: "123-4567",
         table: 1
@@ -14,24 +15,25 @@ const reservations = [
 ]
 const waitingList = [
     {
-        name: "bob",
+        fname: "bob",
+        lname: "smith",
         email: "bob@bob.com",
         phone: "123-4567",
         table: 1
     }
 ]
 app.get("/", function(req,res) {
-    res.sendFile(path.join(___dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 app.get("/make", function(req,res) {
-    res.sendFile(path.join(___dirname, "makeres.html"));
+    res.sendFile(path.join(__dirname, "makeres.html"));
 });
 app.get("/tables", function(req,res) {
-    res.sendFile(path.join(___dirname, "tables.html"));
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 app.get("/api/reservations", function (req, res) {
     res.json(reservations)
-};
+});
 app.get("/api/waitinglist", function(req,res) {
     res.json(waitinglist)
 })
@@ -39,12 +41,17 @@ app.post("/api/tables", function(req,res) {
     const newReservation = req.body;
     console.log(newReservation);
     if (reservations.length >= 5) {
-        waitingList.push(newReservation)
+        waitingList.push(newReservation);
     }
     else {
-        reservations.push(newReservation)
+        reservations.push(newReservation);
     }
+    res.end();
 });
 app.post("/cleartables", function(req, res) {
     reservations = [];
 });
+
+app.listen(PORT, function() {
+    console.log("App listening at http://localhost:" + PORT)
+})
