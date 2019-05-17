5967154734661586abd379fe4e113006e12be2c5
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-05-17 11:03:16 -0400

    initial server file and package.json

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 8080;
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+const reservations = [
+    {
+        name: "bob",
+        email: "bob@bob.com",
+        phone: "123-4567",
+        table: 1
+    }
+]
+const waitingList = [
+    {
+        name: "bob",
+        email: "bob@bob.com",
+        phone: "123-4567",
+        table: 1
+    }
+]
+app.get("/", function(req,res) {
+    res.sendFile(path.join(___dirname, "home.html"));
+});
+app.get("/make", function(req,res) {
+    res.sendFile(path.join(___dirname, "makeres.html"));
+});
+app.get("/tables", function(req,res) {
+    res.sendFile(path.join(___dirname, "tables.html"));
+});
+app.post("/tables", function(req,res) {
+    const newReservation = req.body;
+    console.log(newReservation);
+    if (reservations.length >= 5) {
+        waitingList.push(newReservation)
+    }
+    else {
+        reservations.push(newReservation)
+    }
+});
+app.post("/cleartables", function(req, res) {
+    reservations = [];
+});
